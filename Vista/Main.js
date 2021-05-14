a4521893001b084f26acc44a42940b0b3c09f48a
DanielaAgudelo13 <daniela13agudelo@hotmail.com>
2021-05-14 16:39:52 -0500

    Se agrego un login

--- Vista/Main.js
+++ Vista/Main.js
@@ -1,10 +1,24 @@
 let canvas;
+let emailLogin;
+let passwordLogin;
+let registerLogin;
 
 function setup () {
     canvas = createCanvas (414, 896);
     canvas.parent("micanva");
+    emailLogin = document.querySelector("#emailLogin");
+    passwordLogin = document.querySelector("#passwordLogin");
+    registerLogin = document.querySelector("#registerLogin");
+    handleLogin();
 }
 
 function draw () {
     background (255);
+}
+
+function handleLogin () {
+    registerLogin.addEventListener("click", function() {
+        console.log(emailLogin.value);
+        console.log(passwordLogin.value);
+    })
 }
