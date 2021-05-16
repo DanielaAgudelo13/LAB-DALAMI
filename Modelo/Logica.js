9259bf0fb7f1bb51cfacaa65dd20cd5cadf491a8
Isab0808 <isab0808@gmail.com>
2021-05-16 16:15:49 -0500

    Ocultar y mostrar inputs

--- Modelo/Logica.js
+++ Modelo/Logica.js
@@ -1,11 +1,16 @@
 let emailLogin = document.querySelector("#emailLogin");
 let passwordLogin = document.querySelector("#passwordLogin");
+let loginButton = document.querySelector("#loginButton");
+let loginForm = document.querySelectorAll(".loginForm");
+
 let registerLogin = document.querySelector("#registerLogin");
 let emailRegister = document.querySelector("#emailRegister");
 let passwordRegister = document.querySelector("#passwordRegister");
 let cellphoneRegister = document.querySelector("#cellphoneRegister");
 let addressRegister = document.querySelector("#addressRegister");
 let registerButton = document.querySelector("#registerButton");
+let registerForm = document.querySelectorAll(".registerForm");
+
 
 let pantalla = 0;
 
@@ -22,12 +27,12 @@ class Logica {
         switch (pantalla) {
             case 0:// Login
                 image(this.pantallaLogin, 0, 0);
+                this.ocultarTodo(registerForm);
                 break;
             case 1:// Registro
                 image(this.pantallaRegister, 0, 0);
-                this.ocultarElemento(emailLogin);
-                this.ocultarElemento(passwordLogin);
-                this.ocultarElemento(registerLogin);
+                this.ocultarTodo(loginForm);
+                this.mostrarTodo(registerForm);
                 break;
 
         }
@@ -51,7 +56,19 @@ class Logica {
         htmlElement.classList.add("hidden");
     }
 
-    mostrarElemento() {
+    mostrarElemento(htmlElement) {
         htmlElement.classList.remove("hidden");
+    }
+
+    ocultarTodo(list){
+        list.forEach(element => {
+            this.ocultarElemento(element);
+        });
+    }
+
+    mostrarTodo(list){
+        list.forEach(element => {
+            this.mostrarElemento(element);
+        });
     }
 }
